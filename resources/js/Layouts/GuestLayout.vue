931b2706d1cd7f79c596aaaa0eea802ed82bbdfa
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-07-27 19:31:03 +0800

    Update guess UI

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,12 +5,12 @@ import image from './../../images/zoo.svg';
 
 <template>
     <div class="min-h-screen bg-gray-50 grid grid-cols-2">
-        <div class="col-span-1 hidden lg:block">
+        <div class="col-span-1 hidden lg:flex justify-center items-center">
             <img :src="image" />
         </div>
 
         <div class="col-span-2 lg:col-span-1 bg-white flex justify-center items-center">
-            <div class="w-full mx-4 md:w-2/3 shadow-lg bg-gray-50 rounded p-4 relative z-10">
+            <div class="w-full mx-4 md:w-2/3 max-w-md shadow-lg bg-gray-50 rounded p-4 relative z-10">
                 <slot />
             </div>
         </div>
